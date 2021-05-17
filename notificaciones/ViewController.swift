cf99d29908cf09a28e32ea46f93a310b6a1743e5
japocoinyenhoang <japocoin@gmail.com>
2021-05-17 19:12:11 +0200

    first notification app added

---
+++ notificaciones/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  notificaciones
+//
+//  Created by Tardes on 17/5/21.
+//
+
+import UIKit
+import UserNotifications  //libreria peticion de permisos de notificaciones, y se implementa en el didFinishLaunchingWithOptions ()
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txtTitulo: UITextField!
+    
+    
+    @IBOutlet weak var txtDescripcion: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func enviarNotificacionAction(_ sender: Any) {
+        enviarNotificacion()
+    }
+    
+    func enviarNotificacion() {
+        //1. Crear el triger de la notificacion
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
+        
+        //2. Crear el contenido de la notificacion
+        let notificacion = UNMutableNotificationContent ()
+        notificacion.title = "Título"
+        notificacion.subtitle = "subtitulo"
+        notificacion.body = "texto largo notificacion"
+        notificacion.sound = UNNotificationSound.default
+        
+        //3. Creamos un request
+        let request = UNNotificationRequest(identifier: "notificacion basica", content: notificacion, trigger: trigger)
+        
+        //4. Añadimos el request al centro de notificaciones
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print ("se ha producido un error: \(error)")
+            }
+        }
+    }
+}
+
